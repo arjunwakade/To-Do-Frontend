4269adf2a98a28e05ec999bed930c77ebde6820f
arjunwakade <arjunwakade123@gmail.com>
2025-06-03 22:11:37 +0530

    Changed app.test.js directory to src

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../src/App';
+import App from './App';
 
 describe('To-Do App', () => {
   it('renders the app without crashing', () => {
